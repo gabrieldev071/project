4ad99566618173ccf5b93476f53271d9cc890e73
Gabriel Souza <imgabriel.souza071@gmail.com>
2023-08-07 21:00:14 -0300

    update files and branch

--- assets/js/mobile-navbar.js
+++ assets/js/mobile-navbar.js
@@ -4,8 +4,8 @@ const bntMobile = document.querySelector('[data-header-nav-button]');
 bntMobile.addEventListener('click', toggleMenu);
 
 function toggleMenu() {
-    let active = "active"
+    /* let active = "active" */
     const nav = document.querySelector('[data-header-nav]');
     nav.classList.toggle('active') // adiciona caso não tenha e remova caso tenha
-    alert('teste');
+    
 }
